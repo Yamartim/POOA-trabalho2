1920136d3a364eeac276056e1c3ceb93539cd6c2
Yamartim <54243954+Yamartim@users.noreply.github.com>
2020-11-30 20:10:26 -0300

    Update Parser.py

--- src/Parser.py
+++ src/Parser.py
@@ -1,1 +1,19 @@
 from bs4 import BeautifulSoup
+
+class Parser(html):
+    def __init__(self, html):
+        self._html = html
+        _soup = BeautifulSoup(_html, 'html.parser')
+
+class ParserG1(Parser):
+    pass
+
+class ParserUOL(Parser):
+    pass
+
+class ParserFactory:
+    def create_downloader(self, name, html):
+        if name == 'G1':
+            return ParserG1(html)
+        elif name == 'G1':
+            return ParserUOL(html)
