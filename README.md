60375645d2669efff25c86092045a69f6cbdf92c
Iago Elias <iagoelifa@gmail.com>
2020-11-30 19:50:47 -0300

    added readme

---
+++ README.md
@@ -0,0 +1,6 @@
+Download da páginas (urllib, download de maneiras diferentes)
+
+Parsing - dependente de site de notícias -> (pegar o arquivo html e interpretar dado o tipo do site de notícias)
+(Estadão, Globo, UOL) -> nosso tipo de informação
+
+Parser (nosso tipo -> output correto)
---
+++ src/Downloader.py
@@ -0,0 +1,21 @@
+from urllib.request import urlopen
+import ssl
+from abc import abstractmethod
+
+class Downloader():
+    def __init__(self, url: str):
+        self._url = url
+
+    @abstractmethod
+    def download (self):
+        # to overwrite
+        pass
+
+class DownloaderUrlLib(Downloader):
+    def __init__(self, url: str):
+        super(url)
+    def download (self):
+        ctx = ssl.create_default_context()
+        ctx.check_hostname = False
+        ctx.verify_mode = ssl.CERT_NONE
+        return urlopen(self._url, context=ctx).read()
