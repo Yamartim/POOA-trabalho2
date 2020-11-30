87523dd723062e877348d32f2cb300a1cc6b4fdc
Iago Elias <iagoelifa@gmail.com>
2020-11-30 20:42:09 -0300

    Downloader and Forwarder classes documented

--- src/Downloader.py
+++ src/Downloader.py
@@ -3,22 +3,35 @@ import ssl
 from abc import abstractmethod
 
 class Downloader():
+    '''
+    Class to download a html file 
+    child must define the download implementation
+    '''
     def __init__(self, url: str):
+        '''
+        @param: url to be downloaded as html
+        '''
         self._url = url
-
     def download (self):
-        # to overwrite
+        "to be overloaded"
         pass
 
 class DownloaderUrlLib(Downloader):
+    '''
+    Class to download a html file using urllib
+    '''
     def __init__(self, url: str):
         super().__init__(url)
     def download (self):
+        "Download body of url as html"
         ctx = ssl.create_default_context()
         ctx.check_hostname = False
         ctx.verify_mode = ssl.CERT_NONE
         return urlopen(self._url, context=ctx).read()
 
+'''
+Returns the correct downloader class depending on the name param
+'''
 class DownloaderFactory:
     def create_downloader(self, name, url):
         if name == 'urllib':
--- src/Forwarder.py
+++ src/Forwarder.py
@@ -1,25 +1,51 @@
 import csv
 
 class Forwarder():
-    def __init__(self, results):
+    '''
+    Class to retrieve a list of results and 
+    forward it in any way (depending on the child implementation).
+    '''
+    def __init__(self, results: list):
+        '''
+        @param: results the list of results
+        '''
         self._results = results
-    def foward(self):
+    def forward(self):
+        "method to be overloaded"
         pass
 
 class ForwarderCSV(Forwarder):
+    '''
+    Class to foward a list as a csv file
+    '''
     def __init__(self, results, fl=None):
+        '''
+        @param: results the list of results
+        @param: fl opened stream to write as csv
+        '''
         super().__init__(results)
         self._file = fl
     def forward(self):
+        "Creates csv writter and write every list item as a row"
         wr = csv.writer(self._file, quoting=csv.QUOTE_ALL)
         wr.writerows(self._results)
 
 class ForwarderStdout(Forwarder):
+    '''
+    Class to foward a list on stdout
+    '''
     def __init__(self, results):
+        '''
+        @param: results the list of results
+        '''
         super().__init__(results)
     def forward(self):
+        "to print results"
         print (self._results)
 
+'''
+Returns the correct forwarder class depending on the name param
+'''
 class ForwarderFactory():
     def create_forwarder(self, name, results, **kwargs):
         if name == 'csv':
