d301941113f4d76c63beb29016bf180d5ae17736
Yamartim <54243954+Yamartim@users.noreply.github.com>
2020-11-30 20:34:33 -0300

    Create BStest.py

---
+++ BStest.py
@@ -0,0 +1,11 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import ssl
+
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+html = urlopen('https://g1.globo.com/', context=ctx).read()
+soup = BeautifulSoup(html, 'html.parser')
+
+print(soup.findAll())
