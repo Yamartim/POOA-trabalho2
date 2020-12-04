7a93cd5c4c1e25383efd645ecaa3f97894c0d76d
Yamartim <54243954+Yamartim@users.noreply.github.com>
2020-12-04 17:53:17 -0300

    restaurei os comentarios na main

--- main.py
+++ main.py
@@ -1,33 +1,43 @@
-from src import DownloaderFactory, ParserFactory, ForwarderFactory
-
-#URL = 'https://g1.globo.com'
-URL = 'https://www.uol.com.br'
-
-# OUTPUT = 'stdout'
-OUTPUT = 'csv'
-
-if __name__ == '__main__':
-    
-    fac = DownloaderFactory()
-    downloader = fac.create_downloader('urllib', URL)
-
-    html = downloader.download()
-    
-    fac = ParserFactory()
-    parser = fac.create_parser(URL, html = html)
-    #res = [['titulo1'], ['titulo2'], ['titulo3']]
-    res = parser.get_news()
-
-    fac = ForwarderFactory()
-
-    if OUTPUT == 'csv':
-        forwarder = fac.create_forwarder(OUTPUT, results=res, fl=open('file.csv', 'w'))
-    else:
-        forwarder = fac.create_forwarder(OUTPUT, results=res)
-
-    forwarder.forward()
-
 '''
 Iago Elias
 Martim Lima
+
+ENPE 2020 bloco B
+POOA - trabalho 2
 '''
+from src import DownloaderFactory, ParserFactory, ForwarderFactory
+
+#to switch between different settings for download method, news source and output set the values for the DOWNLOAD, URL, OUTPUT and OUTPUT_FILE constants below.
+#new values for the constants should be set if you plan to extend the functionality
+
+DOWNLOAD = 'urllib'
+
+URL = 'https://g1.globo.com'
+#URL = 'https://www.uol.com.br'
+
+#OUTPUT = 'stdout'
+OUTPUT = 'csv'
+
+OUTPUT_FILE = 'file.csv'
+
+if __name__ == '__main__':
+    
+    #the chosen downloader is instantiated and returns an html file
+    fac = DownloaderFactory()
+    downloader = fac.create_downloader(DOWNLOAD, URL)
+
+    html = downloader.download()
+    
+    #the chosen parser is instantiated and parses the obtained html file, obtaining a results list
+    fac = ParserFactory()
+    parser = fac.create_parser(URL, html = html)
+    
+    #dummy results list for testing
+    #res = [['titulo1'], ['titulo2'], ['titulo3']]
+    res = parser.get_news()
+
+    #the chosen forwarder is instantiated and forwards the results list to the desired output
+    fac = ForwarderFactory()
+
+    forwarder = fac.create_forwarder(OUTPUT, results=res, fl=open(OUTPUT_FILE, 'w'))
+    forwarder.forward()
